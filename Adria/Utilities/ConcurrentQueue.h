b95dacb8ddcb3ec1df82b3a4c04645ade183bb5b
Mate <mate.buljan1@gmail.com>
2021-07-05 18:10:26 +0200

    added some of utils to namespace

--- Adria/Utilities/ConcurrentQueue.h
+++ Adria/Utilities/ConcurrentQueue.h
@@ -4,68 +4,73 @@
 #include <mutex>
 #include <condition_variable>
 
-
-template<typename T>
-class ConcurrentQueue
+namespace adria
 {
-public:
 
-    ConcurrentQueue() = default;
-    ~ConcurrentQueue() = default;
-
-    ConcurrentQueue(ConcurrentQueue const&) = delete;
-    ConcurrentQueue(ConcurrentQueue&&) = delete;
-
-    ConcurrentQueue& operator=(ConcurrentQueue const&) = delete;
-    ConcurrentQueue& operator=(ConcurrentQueue&&) = delete;
-
-
-    void Push(T const& value)
+    template<typename T>
+    class ConcurrentQueue
     {
-        std::lock_guard<std::mutex> lock(mutex);
-        queue.push(std::move(value));
-        condVar.notify_one();
-    }
+    public:
 
-    void Push(T&& value)
-    {
-        std::lock_guard<std::mutex> lock(mutex);
-        queue.push(value);
-        condVar.notify_one();
-    }
+        ConcurrentQueue() = default;
+        ~ConcurrentQueue() = default;
 
-    void WaitPop(T& value)
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        condVar.wait(lock, [this] {return !queue.empty(); });
-        value = std::move(queue.front());
-        queue.pop();
-    }
+        ConcurrentQueue(ConcurrentQueue const&) = delete;
+        ConcurrentQueue(ConcurrentQueue&&) = delete;
 
-    bool TryPop(T& value)
-    {
-        std::lock_guard<std::mutex> lock(mutex);
-        if (queue.empty()) return false;
+        ConcurrentQueue& operator=(ConcurrentQueue const&) = delete;
+        ConcurrentQueue& operator=(ConcurrentQueue&&) = delete;
 
-        value = std::move(queue.front());
-        queue.pop();
-        return true;
-    }
 
-    bool Empty() const
-    {
-        std::lock_guard<std::mutex> lock(mutex);
-        return queue.empty();
-    }
+        void Push(T const& value)
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            queue.push(std::move(value));
+            condVar.notify_one();
+        }
 
-    size_t Size() const
-    {
-        std::lock_guard<std::mutex> lock(mutex);
-        return queue.size();
-    }
+        void Push(T&& value)
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            queue.push(value);
+            condVar.notify_one();
+        }
 
-private:
-    std::queue<T> queue;
-    mutable std::mutex mutex;
-    std::condition_variable condVar;
-};
+        void WaitPop(T& value)
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            condVar.wait(lock, [this] {return !queue.empty(); });
+            value = std::move(queue.front());
+            queue.pop();
+        }
+
+        bool TryPop(T& value)
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            if (queue.empty()) return false;
+
+            value = std::move(queue.front());
+            queue.pop();
+            return true;
+        }
+
+        bool Empty() const
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            return queue.empty();
+        }
+
+        size_t Size() const
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            return queue.size();
+        }
+
+    private:
+        std::queue<T> queue;
+        mutable std::mutex mutex;
+        std::condition_variable condVar;
+    };
+
+
+}
--- Adria/Utilities/ObjectPool.h
+++ Adria/Utilities/ObjectPool.h
@@ -3,111 +3,114 @@
 #include <memory>
 #include <cassert>
 
-
-
-template<typename T>
-class ObjectPool
+namespace adria
 {
 
-    union Chunk
+    template<typename T>
+    class ObjectPool
     {
-        using StorageType = char[sizeof(T)];
 
-        Chunk* next = nullptr;
+        union Chunk
+        {
+            using StorageType = char[sizeof(T)];
 
-        StorageType object;
+            Chunk* next = nullptr;
+
+            StorageType object;
+        };
+
+
+    public:
+        ObjectPool(size_t size) : pool_size{ size }, used{ 0 }
+        {
+
+            mem_alloc = reinterpret_cast<Chunk*>(malloc(sizeof(Chunk)));
+
+            Chunk* tmp = mem_alloc;
+
+            for (size_t i = 0; i < size - 1; ++i)
+            {
+                tmp->next = reinterpret_cast<Chunk*>(malloc(sizeof(Chunk)));
+
+                tmp = tmp->next;
+            }
+        }
+
+        ObjectPool(ObjectPool const&) = delete;
+        ObjectPool(ObjectPool&&) = delete;
+
+        ~ObjectPool()
+        {
+            Chunk* curr_alloc = mem_alloc;
+            while (curr_alloc != nullptr)
+            {
+                Chunk* temp = curr_alloc;
+                curr_alloc = curr_alloc->next;
+                free(temp);
+            }
+            mem_alloc = nullptr;
+        }
+
+        [[nodiscard]] void* Allocate()
+        {
+            if (mem_alloc == nullptr)
+                return nullptr;
+
+            Chunk* free_chunk = mem_alloc;
+
+            mem_alloc = mem_alloc->next;
+
+            used += sizeof(Chunk);
+
+            return free_chunk;
+        }
+
+        void Free(void* chunk)
+        {
+
+            Chunk* new_start_ptr = reinterpret_cast<Chunk*>(chunk);
+
+            new_start_ptr->next = mem_alloc;
+
+            mem_alloc = new_start_ptr;
+
+            used -= sizeof(Chunk);
+        }
+
+        template<typename T, typename ...Args>
+        T* Construct(Args&& ...args)
+        {
+            return new (Allocate()) T(std::forward<Args>(args)...);
+        }
+
+        template<typename T>
+        void Destroy(T* p) noexcept
+        {
+            if (p == nullptr) return;
+
+            Free(p);
+
+            p->~T();
+        }
+
+        size_t GetMemorySize() const
+        {
+            return pool_size * sizeof(Chunk);
+        }
+
+        size_t GetUsedMemorySize() const
+        {
+            return used;
+        }
+
+    private:
+        Chunk* mem_alloc = nullptr;
+        size_t const pool_size;
+        size_t used;
+
     };
 
-
-public:
-    ObjectPool(size_t size) : pool_size{ size }, used{ 0 }
-    {
-
-        mem_alloc = reinterpret_cast<Chunk*>(malloc(sizeof(Chunk)));
-
-        Chunk* tmp = mem_alloc;
-
-        for (size_t i = 0; i < size - 1; ++i)
-        {
-            tmp->next = reinterpret_cast<Chunk*>(malloc(sizeof(Chunk)));
-
-            tmp = tmp->next;
-        }
-    }
-
-    ObjectPool(ObjectPool const&) = delete;
-    ObjectPool(ObjectPool&&) = delete;
-
-    ~ObjectPool()
-    {
-        Chunk* curr_alloc = mem_alloc;
-        while (curr_alloc != nullptr)
-        {     
-            Chunk* temp = curr_alloc;
-            curr_alloc = curr_alloc->next;
-            free(temp);
-        }
-        mem_alloc = nullptr;
-    }
-
-    [[nodiscard]] void* Allocate()
-    {
-        if (mem_alloc == nullptr)
-            return nullptr;
-
-        Chunk* free_chunk = mem_alloc;
-
-        mem_alloc = mem_alloc->next;
-
-        used += sizeof(Chunk);
-
-        return free_chunk;
-    }
-
-    void Free(void* chunk)
-    {
-
-        Chunk* new_start_ptr = reinterpret_cast<Chunk*>(chunk);
-
-        new_start_ptr->next = mem_alloc;
-
-        mem_alloc = new_start_ptr;
-
-        used -= sizeof(Chunk);
-    }
-
-    template<typename T, typename ...Args>
-    T* Construct(Args&& ...args)
-    {
-        return new (Allocate()) T(std::forward<Args>(args)...);
-    }
-
-    template<typename T>
-    void Destroy(T* p) noexcept
-    {
-        if (p == nullptr) return;
-            
-        Free(p);
-
-        p->~T();
-    }
-
-    size_t GetMemorySize() const
-    {
-        return pool_size * sizeof(Chunk);
-    }
-
-    size_t GetUsedMemorySize() const
-    {
-        return used;
-    }
-
-private:
-    Chunk* mem_alloc = nullptr;
-    size_t const pool_size;
-    size_t used;
-
-};
+}
 
 
 
