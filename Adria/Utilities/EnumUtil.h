3d2b9c4b7fa0c4bd8b3126c5d3f4bdff5219a9c8
Mate Buljan <mate.buljan1@gmail.com>
2022-03-09 13:10:43 +0100

    Fix enum operators

--- Adria/Utilities/EnumUtil.h
+++ Adria/Utilities/EnumUtil.h
@@ -1,15 +1,18 @@
 #pragma once
+#include <type_traits>
 
 namespace adria
 {
 
 #define DEFINE_ENUM_BIT_OPERATORS(ENUMTYPE) \
-inline ENUMTYPE operator | (ENUMTYPE a, ENUMTYPE b) { return ENUMTYPE(((int)a) | ((int)b)); } \
-inline ENUMTYPE &operator |= (ENUMTYPE &a, ENUMTYPE b) { return (ENUMTYPE &)(((int &)a) |= ((int)b)); } \
-inline ENUMTYPE operator & (ENUMTYPE a, ENUMTYPE b) { return ENUMTYPE(((int)a) & ((int)b)); } \
-inline ENUMTYPE &operator &= (ENUMTYPE &a, ENUMTYPE b) { return (ENUMTYPE &)(((int &)a) &= ((int)b)); } \
-inline ENUMTYPE operator ~ (ENUMTYPE a) { return ENUMTYPE(~((int)a)); } \
-inline ENUMTYPE operator ^ (ENUMTYPE a, ENUMTYPE b) { return ENUMTYPE(((int)a) ^ ((int)b)); } \
-inline ENUMTYPE &operator ^= (ENUMTYPE &a, ENUMTYPE b) { return (ENUMTYPE &)(((int &)a) ^= ((int)b)); } \
+static_assert(std::is_enum_v<ENUMTYPE>); \
+using TYPE = std::underlying_type_t<ENUMTYPE>; \
+inline ENUMTYPE  operator|(ENUMTYPE a, ENUMTYPE b) { return ENUMTYPE(((TYPE)a) | ((TYPE)b)); } \
+inline ENUMTYPE& operator|=(ENUMTYPE& a, ENUMTYPE b) { return (ENUMTYPE &)(((TYPE&)a) |= ((TYPE)b)); } \
+inline ENUMTYPE  operator&(ENUMTYPE a, ENUMTYPE b) { return ENUMTYPE(((TYPE)a) & ((TYPE)b)); } \
+inline ENUMTYPE& operator&=(ENUMTYPE& a, ENUMTYPE b) { return (ENUMTYPE &)(((TYPE&)a) &= ((TYPE)b)); } \
+inline ENUMTYPE  operator~(ENUMTYPE a) { return ENUMTYPE(~((TYPE)a)); } \
+inline ENUMTYPE  operator^(ENUMTYPE a, ENUMTYPE b) { return ENUMTYPE(((TYPE)a) ^ ((TYPE)b)); } \
+inline ENUMTYPE& operator^=(ENUMTYPE& a, ENUMTYPE b) { return (ENUMTYPE &)(((TYPE&)a) ^= ((TYPE)b)); }
 
 }
