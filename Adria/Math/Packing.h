5266a20d9c9f1a96f94dbf74a6a3553779f318aa
Mate Buljan <mate.buljan1@gmail.com>
2022-10-18 14:49:32 +0200

    added some packing functions, not tested

--- Adria/Math/Packing.h
+++ Adria/Math/Packing.h
@@ -9,22 +9,9 @@ namespace adria
 		return value > max ? max : (value < min ? min : value);
 	}
 
-	inline uint32 PackToUint(float32 r, float32 g, float32 b, float32 a = 1.0f)
-	{
-		uint32 output = 0;
-		output |= (uint8)(Clamp(r) * 255.0f) << 24;
-		output |= (uint8)(Clamp(g) * 255.0f) << 16;
-		output |= (uint8)(Clamp(b) * 255.0f) << 8;
-		output |= (uint8)(Clamp(a) * 255.0f) << 0;
-		return output;
-	}
-	inline uint32 PackToUint(float32(&arr)[3])
-	{
-		uint32 output = 0;
-		output |= (uint8)(Clamp(arr[0]) * 255.0f) << 24;
-		output |= (uint8)(Clamp(arr[1]) * 255.0f) << 16;
-		output |= (uint8)(Clamp(arr[2]) * 255.0f) << 8;
-		output |= (uint8)(255.0f) << 0;
-		return output;
-	}
+	uint32 PackToUint(float32 r, float32 g, float32 b, float32 a = 1.0f);
+	uint32 PackToUint(float32(&arr)[3]);
+
+	uint32 PackTwoFloatsToUint32(float32 x, float32 y);
+	uint64 PackFourFloatsToUint64(float32 x, float32 y, float32 z, float32 w);
 }
